61ae20a7f425fe7aacbb1ccb65dc1d409461d618
nagasowmyavalmiki <nagasowmya.valmiki@gmail.com>
2020-02-15 18:33:50 +0530

    comming all screen changes

---
+++ README.md
@@ -0,0 +1,19 @@
+WouldYouRather Project:
+user play the “Would You Rather?” game. The game goes like this: A user is asked a question in the form: “Would you rather (option A) or (option B) ?”. 
+Answering "neither" or "both" is against the rules. users will be able to answer questions, see which questions they haven’t answered, 
+see how other people have voted, post questions, and see the ranking of users on the leaderboard. 
+As a result you will notice that data does not persist when the browser is refreshed.
+
+
+How to Run:
+Run npm install to install the project dependencies.
+Run the app using npm start.
+App can be seen at: localhost:3000.
+
+
+How to Use the App:
+You start login to the app by selecting a user from the login page dropdown. You will see Home, NewQuestion, Leaderboard, Logout things in navigation bar.
+Home will be like dash boad type of thing, categorized as UnAswered Questions, Answered Questions. Use "View Poll" option to navigate to vote. 
+NewQuestion: You can also add a question using this tab. Leader board shows how many questions each user has asked and answered?,
+Users will be ordered in descending order based on the sum of the number of questions they’ve asked and the number of questions they’ve answered.
+At last you will notice that data does not persist when the browser is refreshed.
---
+++ src/utils/_DATA.js
@@ -0,0 +1,206 @@
+let users = {
+  sowmyavalmiki: {
+    id: 'sowmyavalmiki',
+    name: 'Sowmya',
+    avatarURL: '../../images/sowmya.jpg',
+    answers: {
+      "8xf0y6ziyjabvozdd253nd": 'optionOne',
+      "6ni6ok3ym7mf1p33lnez": 'optionOne',
+      "am8ehyc8byjqgar0jgpub9": 'optionTwo',
+      "loxhs1bqm25b708cmbf3g": 'optionTwo'
+    },
+    questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
+  },
+  venkyborra: {
+    id: 'venkyborra',
+    name: 'Venky',
+    avatarURL: '../../images/venky.jpg',
+    answers: {
+      "vthrdm985a262al8qx3do": 'optionOne',
+      "xj352vofupe1dqz9emx13r": 'optionTwo',
+    },
+    questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do'],
+  },
+  bhavyavalmiki: {
+    id: 'bhavyavalmiki',
+    name: 'Bhavya',
+    avatarURL: '../../images/bhavya.jpg',
+    answers: {
+      "xj352vofupe1dqz9emx13r": 'optionOne',
+      "vthrdm985a262al8qx3do": 'optionTwo',
+      "6ni6ok3ym7mf1p33lnez": 'optionOne'
+    },
+    questions: ['6ni6ok3ym7mf1p33lnez', 'xj352vofupe1dqz9emx13r'],
+  }
+}
+
+let questions = {
+  "8xf0y6ziyjabvozdd253nd": {
+    id: '8xf0y6ziyjabvozdd253nd',
+    author: 'sowmyavalmiki',
+    timestamp: 1467166872634,
+    optionOne: {
+      votes: ['sowmyavalmiki'],
+      text: 'Follow your heart',
+    },
+    optionTwo: {
+      votes: [],
+      text: 'Follow your mind'
+    }
+  },
+  "6ni6ok3ym7mf1p33lnez": {
+    id: '6ni6ok3ym7mf1p33lnez',
+    author: 'bhavyavalmiki',
+    timestamp: 1468479767190,
+    optionOne: {
+      votes: [],
+      text: 'Work out is good for health',
+    },
+    optionTwo: {
+      votes: ['bhavyavalmiki', 'sowmyavalmiki'],
+      text: 'diet is good for health '
+    }
+  },
+  "am8ehyc8byjqgar0jgpub9": {
+    id: 'am8ehyc8byjqgar0jgpub9',
+    author: 'sowmyavalmiki',
+    timestamp: 1488579767190,
+    optionOne: {
+      votes: [],
+      text: 'Singing gives us relief',
+    },
+    optionTwo: {
+      votes: ['sowmyavalmiki'],
+      text: 'dancing gives us relief'
+    }
+  },
+  "loxhs1bqm25b708cmbf3g": {
+    id: 'loxhs1bqm25b708cmbf3g',
+    author: 'venkyborra',
+    timestamp: 1482579767190,
+    optionOne: {
+      votes: [],
+      text: 'be a front-end developer',
+    },
+    optionTwo: {
+      votes: ['sowmyavalmiki'],
+      text: 'be a back-end developer'
+    }
+  },
+  "vthrdm985a262al8qx3do": {
+    id: 'vthrdm985a262al8qx3do',
+    author: 'venkyborra',
+    timestamp: 1489579767190,
+    optionOne: {
+      votes: ['venkyborra'],
+      text: 'find $50 yourself',
+    },
+    optionTwo: {
+      votes: ['bhavyavalmiki'],
+      text: 'have your best friend find $500'
+    }
+  },
+  "xj352vofupe1dqz9emx13r": {
+    id: 'xj352vofupe1dqz9emx13r',
+    author: 'bhavyavalmiki',
+    timestamp: 1493579767190,
+    optionOne: {
+      votes: ['bhavyavalmiki'],
+      text: 'write JavaScript',
+    },
+    optionTwo: {
+      votes: ['venkyborra'],
+      text: 'write Swift'
+    }
+  },
+}
+
+function generateUID () {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
+export function _getUser(id) {
+  return new Promise((res, rej) => {
+      setTimeout(() => res(users[id]), 1000)
+  });
+}
+export function _getUsers () {
+  return new Promise((res, rej) => {
+    setTimeout(() => res({...users}), 1000)
+  })
+}
+
+export function _getQuestions () {
+  return new Promise((res, rej) => {
+    setTimeout(() => res({...questions}), 1000)
+  })
+}
+
+function formatQuestion ({ optionOneText, optionTwoText, author }) {
+  return {
+    id: generateUID(),
+    timestamp: Date.now(),
+    author,
+    optionOne: {
+      votes: [],
+      text: optionOneText,
+    },
+    optionTwo: {
+      votes: [],
+      text: optionTwoText,
+    }
+  }
+}
+
+export function _saveQuestion (question) {
+  return new Promise((res, rej) => {
+    const authedUser = question.author;
+    const formattedQuestion = formatQuestion(question)
+
+    setTimeout(() => {
+      questions = {
+        ...questions,
+        [formattedQuestion.id]: formattedQuestion
+      }
+      
+      users = {
+        ...users,
+        [authedUser]: {
+          ...users[authedUser],
+          questions: users[authedUser].questions.concat([formattedQuestion.id])
+        }
+      }
+
+      res(formattedQuestion)
+    }, 1000)
+  })
+}
+
+export function _saveQuestionAnswer ({ authedUser, qid, answer }) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      users = {
+        ...users,
+        [authedUser]: {
+          ...users[authedUser],
+          answers: {
+            ...users[authedUser].answers,
+            [qid]: answer
+          }
+        }
+      }
+
+      questions = {
+        ...questions,
+        [qid]: {
+          ...questions[qid],
+          [answer]: {
+            ...questions[qid][answer],
+            votes: questions[qid][answer].votes.concat([authedUser])
+          }
+        }
+      }
+
+      res()
+    }, 500)
+  })
+}
